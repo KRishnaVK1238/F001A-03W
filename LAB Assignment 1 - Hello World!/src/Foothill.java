08874a2d68f20f1495ed30f1cf877a01d2215091
KrishnaVKMC <67489477+KrishnaVKMC@users.noreply.github.com>
2020-07-02 09:00:38 -0700

    Lab 1

---
+++ LAB Assignment 1 - Hello World!/src/Foothill.java
@@ -0,0 +1,25 @@
+
+
+public class Foothill {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        System.out.println("Name = Krishna Vamsi Koganti");
+
+        System.out.println("Foothill ID # = 20347083");
+
+        System.out.println();
+
+        System.out.println("Syllabus Details: \n" +
+                "This class should take 20 to 26 hours per week \n" +
+                "each week question adn comments will be dissed in the forums \n" +
+                "If we have any question, we should contact you through Canvas messaging system \n");
+
+        System.out.println("Specific Requirements and Point Penalties: \n" +
+                "Make sure to test your program a lot before you submit it \n" +
+                "Late world is 3 point penalty per day");
+
+
+    }
+}
